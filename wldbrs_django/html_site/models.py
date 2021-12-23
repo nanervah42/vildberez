a2577e9e9cd4a1abca4eb7af18ff94098000d186
nanervah <nanervah@yandex.ru>
2021-12-23 17:46:51 +0700

    #9

--- wldbrs_django/html_site/models.py
+++ wldbrs_django/html_site/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# class MainBase(models.Model):
-#     item_id = models.IntegerField(unique=True, blank=None)
-#     brand_name = models.CharField(max_length=250)
-#     goods_name = models.CharField(max_length=250)
-#     img = models.CharField(max_length=250)
-#     url = models.CharField(max_length=250)
-#     price_now = models.IntegerField(blank=None)
-#     price_min = models.IntegerField(blank=None)
-#     price_max = models.IntegerField(blank=None)
-#     timer = models.BigIntegerField(default=0)
-#     b_count = models.IntegerField(default=0)
-#     stars = models.IntegerField(default=0)
-#     flag = models.BooleanField()
-#     company_name = models.CharField(max_length=50)
-
 
 class wb_base(models.Model):
     item_id = models.BigIntegerField(primary_key=True, unique=True, blank=None)
@@ -32,3 +16,6 @@ class wb_base(models.Model):
     company_name = models.CharField(max_length=50)
     product_type = models.CharField(max_length=50)
     updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_time']
--- wldbrs_django/html_site/views.py
+++ wldbrs_django/html_site/views.py
@@ -7,7 +7,7 @@ from .models import wb_base
 class HomeIndex(ListView):   # вместо index
     model = wb_base        # определяем модель откуда беруться все данные
     template_name = 'wldbrs_django/index.html'   # переопределяем дефолтное название шаблона
-    context_object_name = 'wldbrs'        # переопределяемn дефолтное название объекта
+    context_object_name = 'wldbrs'        # переопределяем дефолтное название объекта
     # paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):  # ...для динамичных данных
