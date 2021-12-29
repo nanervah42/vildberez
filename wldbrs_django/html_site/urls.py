6f1d48f6cbc110891c33cec37dfe19f1b5bad30c
nanervah <nanervah@yandex.ru>
2021-12-29 18:02:02 +0700

    #14

--- wldbrs_django/html_site/urls.py
+++ wldbrs_django/html_site/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomeIndex.as_view(), name='home'),
-    path('category/<str:product_type>/', Category.as_view(), name='category')
+    path('category/<str:product_type>/', Category.as_view(), name='category'),
+    path('telegram/', Telegram.as_view(), name='telegram'),
+    path('about/', About.as_view(), name='about'),
 ]
--- wldbrs_django/html_site/views.py
+++ wldbrs_django/html_site/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from .models import wb_base
 
 
@@ -21,6 +21,7 @@ class HomeIndex(ListView):  # вместо index
     def get_queryset(self):  # правим дефолтный запрос который ~ SELECT все поля FROM таблица, без всяких условий
         return wb_base.objects.all().order_by('-updated_time')
 
+
 class Category(ListView):
     model = wb_base
     template_name = 'wldbrs_django/index.html'
@@ -36,3 +37,36 @@ class Category(ListView):
 
     def get_queryset(self):  # правим дефолтный запрос который ~ SELECT все поля FROM таблица, без всяких условий
         return wb_base.objects.filter(product_type=self.kwargs['product_type']).order_by('-updated_time')
+
+
+# class Telegram(ListView):
+#     model = wb_base
+#     template_name = 'wldbrs_django/telegram.html'
+#     context_object_name = 'wldbrs'
+#     paginate_by = 3
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):  # ...для динамичных данных
+#         context = super().get_context_data(**kwargs)  # получаем контекст, который уже есть
+#         context['title'] = 'Главная страница'  # дополняем его
+#
+#         return context
+
+
+class Telegram(TemplateView):
+    template_name = 'wldbrs_django/telegram.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):  # ...для динамичных данных
+        context = super().get_context_data(**kwargs)  # получаем контекст, который уже есть
+        context['title'] = 'Телеграм'  # дополняем его
+
+        return context
+
+
+class About(TemplateView):
+    template_name = 'wldbrs_django/about.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):  # ...для динамичных данных
+        context = super().get_context_data(**kwargs)  # получаем контекст, который уже есть
+        context['title'] = 'О Проекте'  # дополняем его
+
+        return context
